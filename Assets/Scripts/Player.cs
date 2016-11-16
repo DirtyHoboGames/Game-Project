79931a5008107e14771c57f51351de827a6e43d1
DirtyHoboGames <erkkiha@metropolia.fi>
2016-11-16 14:15:20 +0200

    Added Player Class which has stats and name

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts {
+    class Player {
+
+        string playerName;
+        int Intelligence;
+        int Charisma;
+        int Strength
+
+        public Player(string playerName, int Int, int Char, int Str, int Lck) {
+
+            
+
+        }
+
+    }
+}
