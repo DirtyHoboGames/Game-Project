188f6d78e42f66027f2d7762a657c159e320bff2
DirtyHoboGames <erkkiha@metropolia.fi>
2016-11-29 10:53:20 +0200

    MOVING GARDS lmao -mmmm

--- Assets/Scripts/GuardMovement.cs
+++ Assets/Scripts/GuardMovement.cs
@@ -3,38 +3,33 @@ using System.Collections;
 
 public class GuardMovement : MonoBehaviour {
 
-    Transform target;
-    Transform target2;
     public float speed;
-    private float minDistance = 0.1f;
-    private float maxDistance = 20;
-    private float range;
-    private float range2;
     private Animator animator;
     private bool flipGuard = true;
+    private Rigidbody2D guardBody;
+
 
     void Start() {
         animator = GetComponent<Animator>();
+        guardBody = GetComponent<Rigidbody2D>();
+        guardBody.velocity = new Vector2(speed,0);
+        animator.SetBool("GHorizAnim", true);
+    }
+
+    void OnTriggerEnter2D(Collider2D colli) {
+        if (colli.gameObject.tag=="Waypoint1") {
+            guardBody.velocity = new Vector2(speed,0);
+            GuardFlip(1);
+        }
+        if (colli.gameObject.tag == "Waypoint2") {
+            guardBody.velocity = new Vector2(-speed, 0);
+            GuardFlip(-1);
+            
+        }
     }
 
     void Update() {
-        target = GameObject.FindGameObjectWithTag("Waypoint2").transform;
-        target2 = GameObject.FindGameObjectWithTag("Waypoint1").transform;
-        range = Vector2.Distance(transform.position, target.position);
-        range2 = Vector2.Distance(transform.position, target2.position);
-
-
-        if (range <= maxDistance) { //moves the guard to waypoint (target = waypoint2)
-            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
-            animator.SetBool("GHorizAnim", true);
-            }
-        if (range <= minDistance) {
-            animator.SetBool("GHorizAnim", false);
-        }
-       /* if (range2 <= maxDistance && range2 >= 0.5f) { // doesnt work pls fix
-            transform.position = Vector2.MoveTowards(transform.position, target2.position, speed * Time.deltaTime);
-        }*/
-        
+        transform.rotation = Quaternion.identity;
     }
     private void GuardFlip(int direction) { //Flips the guards animation when going left. 
         if (direction > 0 && !flipGuard || direction < 0 && flipGuard) {
